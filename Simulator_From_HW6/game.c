14199b9690de29516edb89a62eeaac2bb15a7dae
kmfb21 <kmfb21@gmail.com>
2016-04-18 21:28:33 -0400

    AI random move done

--- Simulator_From_HW6/game.c
+++ Simulator_From_HW6/game.c
@@ -8,6 +8,13 @@
 #include <SDL/SDL.h>
 
 Tank user;
+Tank tanks[3];
+uint8_t lives;
+uint8_t positions[3][2] = {
+  {0,0},
+  {48,0},
+  {112,0}
+};
 
 void event_loop(void) {
   static int user_v_move = 0;
@@ -67,30 +74,67 @@ void event_loop(void) {
 void tank_game(void) {
 
   f3d_lcd_drawString(25,150,"hit q to quit",WHITE,BLACK);
+  //0b  0  0  0  1
+  //   -h +v +h -v
+  static int tank_move0 = 0b0100;
+  static int tank_move1 = 0b0100;
+  static int tank_move2 = 0b0100;
+  if(ENEMY0) {
+    if(tanks[0].x%16==0 && tanks[0].y%16==0 && ACTION)
+      tank_move0 = 1<<(rand()%4);
+    moveTank(tanks, tank_h(0), tank_v(0), BLACK);
+  } else {
+    initTank(tanks,positions[0][0],positions[0][1],1);
+    drawTank(tanks);
+    lives|=1;
+  }
+  if(ENEMY1) {
+    if(tanks[1].x%16==0 && tanks[1].y%16==0 && ACTION)
+      tank_move1 = 1<<(rand()%4);
+    moveTank(tanks+1, tank_h(1), tank_v(1), BLACK);
+  } else {
+    initTank(tanks+1,positions[1][0],positions[1][1],1);
+    drawTank(tanks+1);
+    lives|=2;
+  }
+  if(ENEMY2) {
+    if(tanks[2].x%16==0 && tanks[2].y%16==0 && ACTION)
+      tank_move2 = 1<<(rand()%4);
+    moveTank(tanks+2, tank_h(2), tank_v(2), BLACK);
+  } else {
+    initTank(tanks+2,positions[2][0],positions[2][1],1);
+    drawTank(tanks+2);
+    lives|=4;
+  }
   event_loop();
 }
 
 int pressed() {
   SDL_Event event;
+  uint8_t count=0;
   while(1) {
+    count++;
     while(event.type!=SDL_KEYDOWN) SDL_PollEvent(&event);
     while(event.type!=SDL_KEYUP) SDL_PollEvent(&event);
-    if(event.key.keysym.sym==SDLK_n) return 'n';
-    if(event.key.keysym.sym==SDLK_q) return 'q';
+    if(event.key.keysym.sym==SDLK_s) return count;
   }
 }
-//extern uint16_t wallimg[16][16];
 
 int c335_main( int argc, char *argv[] ) {
 
   //f3d_lcd_fillScreen(BLACK);
 
-  initTank(&user,56,128,0);
+  f3d_lcd_drawString(25,150,"hit s toStart",WHITE,BLACK);
+  //use time of user's delay as random seed
+  srand(pressed());
+
+  //f3d_lcd_fillScreen(BLACK);
+  initTank(&user,USER_x,USER_y,0);
   user.head=0;
   drawTank(&user);
   drawMap();
-  //pressed();
-  //return(0);
+  lives=0;
+
   while (1) {
     tank_game();
     Delay(EVENT_LOOP_TIME);
--- Simulator_From_HW6/info.h
+++ Simulator_From_HW6/info.h
@@ -1,6 +1,24 @@
+//cell size
 #define CELL 16
 
+//speed and time
 #define TANK_V_INC 1
 #define TANK_H_INC 1
 #define BULL_V 3
 #define EVENT_LOOP_TIME 20
+
+//born place
+#define USER_x 56
+#define USER_y 128
+
+//is enemy alive
+#define ENEMY0 lives&1
+#define ENEMY1 lives>>1&1
+#define ENEMY2 lives>>2&1
+
+//delta x and y move based on 4 bits random number
+#define tank_h(n) (tank_move##n>>1&1)-(tank_move##n>>3&1)
+#define tank_v(n) (tank_move##n>>2&1)-(tank_move##n&1)
+
+//probablity of AI changes his move
+#define ACTION rand()%3==0
